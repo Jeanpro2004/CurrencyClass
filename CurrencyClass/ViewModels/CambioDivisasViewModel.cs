66538821c15776910a75b859bf026f588a4e71dc
Jeanpro2004 <jean.rodriguez.ortiz@udla.edu.ec>
2025-06-12 08:41:21 -0500

    Implement of Views and Models

--- CurrencyClass/ViewModels/CambioDivisasViewModel.cs
+++ CurrencyClass/ViewModels/CambioDivisasViewModel.cs
@@ -5,11 +5,70 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
 namespace CurrencyClass.ViewModels
 {
     class CambioDivisasViewModel : INotifyPropertyChanged
     {
+
+        private string _valorUSD;
+        private string _valorEUR;
+
+        public string ValorUSD { 
+            get => _valorUSD;
+            set
+            {
+                if (_valorUSD != value)
+                {
+                    _valorUSD = value;
+                    OnPropertyChanged();
+                    ConvertirDeUSDAEuros(); 
+                }
+            }
+        }
+        public string ValorEUR
+        {
+            get => _valorEUR;
+            set
+            {
+                if (_valorEUR != value)
+                {
+                    _valorEUR = value;
+                    OnPropertyChanged();
+                    ConvertirDeEurosAUSD();
+                }
+            }
+        }
+
+        public ICommand MostrarResultadosCommand { get; set; }
+
+        public CambioDivisasViewModel()
+        {
+            MostrarResultadosCommand = new Command(async () => await MostrarResultados());
+
+               
+        }
+
+        public async Task MostrarResultados()
+        {
+            ValorEUR = "0";
+            ValorUSD = "0";
+        }
+
+
+        public void ConvertirDeUSDAEuros()
+        {
+            var valorEuros = Double.Parse(_valorUSD) * 0.86;
+            ValorEUR = valorEuros.ToString("F2");
+        }
+
+        public void ConvertirDeEurosAUSD()
+        {
+            var valorUSD = Double.Parse(_valorEUR) /0.86 ;
+            ValorUSD = valorUSD.ToString("F2");
+        }
+
         public event PropertyChangedEventHandler? PropertyChanged;
 
         public void OnPropertyChanged([CallerMemberName] string name = "") =>
---
+++ CurrencyClass/Views/CambioDivisasView.xaml.cs
@@ -0,0 +1,9 @@
+namespace CurrencyClass.Views;
+
+public partial class CambioDivisasView : ContentPage
+{
+	public CambioDivisasView()
+	{
+		InitializeComponent();
+	}
+}
